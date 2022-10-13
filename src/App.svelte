7aebecfa246ccf6b1463d9cdc833eb1f5e5516b9
jannickeham <jannicke.hamarsnes@gmail.com>
2022-10-13 21:06:56 +0200

    first simple version of cookieclickr

---
+++ src/App.svelte
@@ -0,0 +1,41 @@
+<script>
+  import { scale } from "svelte/transition";
+  import { quintOut } from "svelte/easing";
+
+  let cookieImg = "./src/assets/cookie.png";
+
+  let counter = 0;
+
+  function handleClick() {
+    counter++;
+  }
+
+  function resetClicks() {
+    counter = 0;
+  }
+</script>
+
+<main>
+  <div>
+    <img
+      src={cookieImg}
+      on:click={handleClick}
+      on:keydown={handleClick}
+      transition:scale={{
+        duration: 500,
+        delay: 500,
+        opacity: 0.5,
+        start: 0.5,
+        easing: quintOut,
+      }}
+      alt="Cookie"
+    />
+  </div>
+  <div>
+    <p>{counter}</p>
+  </div>
+  <button on:click={resetClicks}>Reset</button>
+</main>
+
+<style>
+</style>
